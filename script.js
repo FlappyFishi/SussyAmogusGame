3ef3b1e43877561601b20c3c9986df7ba00eae68
John Doe <johndoe@email.com>
2024-12-25 10:43:48 +1000

    added sounds lol

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ let clickCount = 0;
 document.getElementById('cookie').addEventListener('click', () => {
   clickCount++;
   document.getElementById('click-count').innerText = clickCount;
+  playClickSound();
 });
+
+function playClickSound() {
+  const clickSound = new Audio('assets/sounds/shortened monkey.mp3');
+  clickSound.play();
+}
